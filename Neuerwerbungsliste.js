62fe49f39d986e8936db72da5ff11f17f0aad69a
Nobutake Kamiya <nobutake.kamiya@aoi.uzh.ch>
2022-04-27 12:31:21 +0200

    add js-code for magnolia

---
+++ Neuerwerbungsliste.js
@@ -0,0 +1,71 @@
+// Das Variabel YoderM soll "y (dieses Jahr)", "m (dieser Monat)", "ly (letztes Jahr)", "lm (letzter Monat)" oder "l3m (letzte 3 Monate)" eingegeben werden
+function NeuerwerbungPlane(Prefix, YoderM) {
+    
+    // Datum heute
+    const d = new Date(Date.now());
+    const Datum = d.toISOString();
+    // ISO-String z.B. "2022-02-18T13:38:17.783Z"
+    let Jahr = Datum.slice(0,4);
+    //console.log(Jahr);
+    const Monat = Datum.slice(5,7);
+    
+    // Letztes Jahr
+    const ynum = Number(Jahr); 
+    const lynum = ynum - 1;
+    const LetztesJahr = String(lynum);
+    
+    //  Letzter Monat
+    const mnum = Number(Monat);
+    const lm = mnum -1
+    const LetzterMonat = ("0" + lm).slice(-2);
+    
+
+    let Suchstring = "";
+
+    switch (YoderM){
+        case "y":
+            Suchstring = Prefix + Jahr + "*";
+            break;
+        
+        case "m":
+            Suchstring = Prefix + Jahr + Monat + "*";
+            break;
+        
+        case "ly":
+            Suchstring = Prefix + LetztesJahr + "*";
+            break;
+        
+        case "lm":
+            if (Monat == "01"){
+                Suchstring = Prefix + LetztesJahr + "12" + "*";
+            } else {
+                Suchstring = Prefix + Jahr + LetzterMonat + "*";
+            }
+            break;
+        
+        case "l3m":
+            for (let i = 1; i < 4 ; i++) {
+                let xm = mnum - i;
+                console.log(xm);
+                if (xm < 1){
+                    xm = 12 + xm;
+                    Jahr = LetztesJahr;
+                }
+
+                let xm_string;
+
+                if (i == 3){
+                    xm_string = ("0" + xm).slice(-2);
+                    Suchstring += Prefix + Jahr + xm_string + "*";
+                    break;
+                }
+                xm_string = ("0" + xm).slice(-2);
+                Suchstring += Prefix + Jahr + xm_string + "*" + " OR ";
+            }
+            break;
+    }
+    
+    
+	window.open("https://uzb.swisscovery.slsp.ch/discovery/search?tab=41SLSP_UZB_DN_and_CI&search_scope=DN_and_CI&vid=41SLSP_UZB:UZB&offset=0&query=any,contains," + Suchstring);
+}
+
