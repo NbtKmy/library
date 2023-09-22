c1e76c07a7197e7199208fc31f9453738aafb8b6
NbtKmy <nobuta77to99@hotmail.com>
2023-09-22 13:23:06 +0200

    change shellscript

--- shell_scripts/concat_csv.sh
+++ shell_scripts/concat_csv.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #Parameters
-$SourceFolder = "C:\Users\nkamiy\desktop\csv"
-$Output = "C:\Users\nkamiy\desktop\csv\concatlated.csv"
+$SourceFolder = "C:\Users\nkamiy\Desktop\csv"
+$Output = "C:\Users\nkamiy\Desktop\csv\concatlated.csv"
 
 # Get a list of the CSV files in a directory
 $CSVFiles = Get-ChildItem -Path $SourceFolder  -Filter "*.csv" 
 
 # Loop over each CSV file
 ForEach ($CSVFile in $CSVFiles) {
-   Import-Csv $CSVFile.FullName -Encoding utf8NoBOM | Export-Csv -Path $Output -Append -NoTypeInformation
+   Import-Csv $CSVFile.FullName | Export-Csv -Path $Output -Append -NoTypeInformation -Encoding UTF8
 }
 
